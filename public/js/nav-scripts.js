22d95fce44de3355857eb2e4e0a6b6af224e2e7d
MrMendonez <dmendonez@gmail.com>
2016-05-14 11:52:54 -0400

    added social media links to mobile side nav

--- public/js/nav-scripts.js
+++ public/js/nav-scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
   // To initiate side Nav for mobile devices
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav({ 
+    closeOnClick: true
+  });
 
 }); // end document ready function
