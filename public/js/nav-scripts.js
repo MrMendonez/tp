8ffe256a5960e0343ee2e3643d6f67a6ba75e371
MrMendonez <dmendonez@gmail.com>
2016-05-14 01:23:12 -0400

    got side nav to work properly

--- public/js/nav-scripts.js
+++ public/js/nav-scripts.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
 
   // To initiate side Nav for mobile devices
-  $(".button-collapse").sideNav({ 
-    closeOnClick: true
-  });
-
-  $('.tooltipped').tooltip({delay: 50});
+  $(".button-collapse").sideNav();
 
 }); // end document ready function
